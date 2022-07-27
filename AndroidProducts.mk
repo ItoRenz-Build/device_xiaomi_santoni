3b1e96d62df164bee44cb0dccd89ae1e48b34a82
ItoRenz <itorenz@nusantararom.org>
2022-07-27 16:42:45 +0000

    santoni: SpicesOS bringup
    Signed-off-by: ItoRenz <itorenz@nusantararom.org>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/nad_santoni.mk
+    $(LOCAL_DIR)/spiceos_santoni.mk
 
 COMMON_LUNCH_CHOICES := \
-    nad_santoni-userdebug \
-    nad_santoni-user \
-    nad_santoni-eng
+    spiceos_santoni-userdebug \
+    spiceos_santoni-user \
+    spiceos_santoni-eng
