afdba8694e029a42fe9ca7f4cbe8793c8e34a558
ItoRenz <itorenz@nusantararom.org>
2022-08-08 09:09:59 +0000

    santoni: Syberia bringup
    Signed-off-by: ItoRenz <itorenz@nusantararom.org>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/nad_santoni.mk
+    $(LOCAL_DIR)/syberia_santoni.mk
 
 COMMON_LUNCH_CHOICES := \
-    nad_santoni-userdebug \
-    nad_santoni-user \
-    nad_santoni-eng
+    syberia_santoni-userdebug \
+    syberia_santoni-user \
+    syberia_santoni-eng
